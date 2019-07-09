facaf96a5710301ce7c27340e97212f8d98c4681
Chris Marlow <christopher.marlow@shopify.com>
2019-07-09 00:50:03 -0400

    Shader applied to green triangle!

--- svg.js
+++ svg.js
@@ -1,51 +1,49 @@
-// import EffectComposer, {
-//   RenderPass,
-//   ShaderPass,
-// } from '@johh/three-effectcomposer';
+import EffectComposer, {
+  RenderPass,
+  CopyShader,
+  ShaderPass,
+} from '@johh/three-effectcomposer';
 
 import {
-  // Color,
-  // MeshBasicMaterial,
-  // BoxBufferGeometry,
-  // ShapeBufferGeometry,
-  // Mesh,
-  // DoubleSide,
-  // Group,
-  // WebGLRenderer,
-  // SVGLoader,
-  // BoxGeometry,
-  // PerspectiveCamera,
   Scene,
 } from 'three';
 
 import {getRenderer, getCamera, getCube} from './scene'
 
-function main() {
-  const renderer = getRenderer(window.innerHeight, window.innerWidth);
-  const camera = getCamera(window.innerHeight, window.innerWidth);
-  const scene = new Scene();
+let state = {
+  shaderTime: 0,
+};
+
+function init(state) {
+  state.renderer = getRenderer(window, '#app');
+  state.camera = getCamera(window.innerHeight, window.innerWidth);
+  state.scene = new Scene();
 
   const cube = getCube();
+  state.scene.add(cube);
 
-  scene.add(cube);
+  // Shaders
+  state.shaders = {
+    renderPass: new RenderPass(state.scene, state.camera),
+    badTVPass: new ShaderPass(THREE.BadTVShader),
+    copyPass: new ShaderPass(CopyShader),
+  };
 
-  renderer.render(scene, camera);
+  // Building Composer
+  state.composer = new EffectComposer(state.renderer);
+  state.composer.addPass(state.shaders.renderPass);
+  state.composer.addPass(state.shaders.badTVPass);
 }
 
-main();
+function animate(state) {
+  state.shaderTime += 0.1;
+  state.shaders.badTVPass.uniforms[ 'time' ].value =  state.shaderTime;
+  // console.log(`tick ${shaderTime}`)
+  requestAnimationFrame(() => animate(state));
 
-// composer.addPass(renderPass);
-// composer.addPass(badTVPass);
-// badTVPass.renderToScreen = true;
+  state.composer.render(0.1)
+}
 
-// var geometry = new BoxBufferGeometry( 1, 1, 1 );
-// var material = new MeshBasicMaterial( { color: 0xffff00 } );
-// var mesh = new Mesh( geometry, material );
-
-// Effects...?
-// const composer = new EffectComposer(renderer);
-// const renderPass = new RenderPass(scene, camera);
-// const badTVPass = new ShaderPass(THREE.BadTVShader);
-
-// scene.add(mesh)
+init(state);
+animate(state);
 
